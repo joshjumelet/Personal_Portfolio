06701bb33f31f0fea2cd997decc19b432c3ef224
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-07 11:05:46 -0700

    build initial Home component & Contact component

---
+++ client/src/components/Contact.js
@@ -0,0 +1,110 @@
+import { useState } from 'react'
+
+const Contact = () => {
+  const [contacts, setContacts] = useState(false)
+
+  const handleShowContacts = (e) => {
+    e.preventDefault()
+    setContacts(!contacts)
+  }
+
+  return (
+    <div className="contact-sidebar">
+      <div className="sidebar">
+        <div className="sidebar-img">
+          <img
+            src="https://i.imgur.com/9vJoA8w.png?1"
+            alt="Joshua Jumelet"
+            width="150"
+          />
+        </div>
+        <div className="sidebar-info">
+          <h1 className="name">Joshua Jumelet</h1>
+          <p>Full Stack Software Engineer</p>
+        </div>
+        <button
+          className="sidebar-more-info"
+          type="button"
+          onClick={handleShowContacts}
+        >
+          Show Contacts
+        </button>
+        {contacts && (
+          <div className="sidebar-contacts">
+            <ul className="contacts-list">
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">Email</p>
+                  <a
+                    href="mailto:joshuajumelet@gmail.com"
+                    className="contact-link"
+                  >
+                    joshuajumelet@gmail.com
+                  </a>
+                </div>
+              </li>
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">Phone</p>
+                  <a href="tel:+13603676583" className="contact-link">
+                    +1 (360) 367-6583
+                  </a>
+                </div>
+              </li>
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">Birthday</p>
+                  <time dateTime="1993-03-37">March 31, 1993</time>
+                </div>
+              </li>
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">Location</p>
+                  <address>
+                    Greater Seatle Area (Granite Falls), Washington
+                  </address>
+                </div>
+              </li>
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">GitHub</p>
+                  <a
+                    className="josh-git"
+                    href="https://github.com/joshjumelet"
+                    target="blank"
+                  >
+                    <img
+                      className="github"
+                      style={{ width: 30, height: 30 }}
+                      src="https://i.imgur.com/AjmoOeq.png"
+                      alt="github"
+                    />
+                  </a>
+                </div>
+              </li>
+              <li className="contacts-item">
+                <div className="contacts-info">
+                  <p className="contacts-title">GitHub</p>
+                  <a
+                    className="josh-linkedIn"
+                    href="https://www.linkedin.com/in/joshua-jumelet/"
+                    target="blank"
+                  >
+                    <img
+                      className="linkedIn"
+                      style={{ width: 30, height: 30 }}
+                      src="https://i.imgur.com/N4ceP5A.png"
+                      alt="linkedIn"
+                    />
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import Contact from './Contact'
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div className="title">
+        <h1>Hi! Welcome to my PORTFOLIO</h1>
+      </div>
+      <div className="contact-sidebar">
+        <Contact />
+      </div>
+    </div>
+  )
+}
+
+export default Home
