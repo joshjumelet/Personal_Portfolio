561c592d3b382df113823185ceb185773eca4253
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-10 10:49:51 -0700

    build projects component with link to deployed projects

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -9,7 +9,55 @@ const Project = () => {
       <header>
         <h1 className="project-title">Projects</h1>
       </header>
-      <section></section>
+      <section>
+        <div className="project-grid">
+          <div className="project-item">
+            <p className="project-title">Thrill RideShare</p>
+            <a
+              className="project-link"
+              href="https://thrill-ride-share.herokuapp.com/"
+              target="blank"
+            >
+              <img
+                className="project-img"
+                style={{ width: 500, height: 300 }}
+                src="https://i.imgur.com/s8KHd0e.jpg"
+                alt="Thrill RideShare"
+              />
+            </a>
+          </div>
+          <div className="project-item">
+            <p className="project-title">Candy Land Game</p>
+            <a
+              className="project-link"
+              href="https://candy-land-game.surge.sh/index.html"
+              target="blank"
+            >
+              <img
+                className="project-img"
+                style={{ width: 500, height: 300 }}
+                src="https://i.imgur.com/PbERsGX.jpg"
+                alt="Candy Land Game"
+              />
+            </a>
+          </div>
+          <div className="project-item">
+            <p className="project-title">Tic Tac Toe</p>
+            <a
+              className="project-link"
+              href="https://tic-tac-toe-josh.surge.sh/"
+              target="blank"
+            >
+              <img
+                className="project-img"
+                style={{ width: 500, height: 300 }}
+                src="https://i.imgur.com/msCy22f.jpg"
+                alt="Tic Tac Toe"
+              />
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
